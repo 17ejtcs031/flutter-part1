0ed79349df897e9f53a310bf1d3d9695feebfffb
Himani Agarwal <52791723+17ejtcs031@users.noreply.github.com>
2020-07-26 00:31:23 +0530

    main file

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:app1/ui/home.dart';
+
+main() {
+  //runapp() method use to run the app it is pre-build method.
+  //In runapp method we call myapp() and run all the statements written in myapp() method
+
+  runApp(myapp());
+}
+
+//https://raw.githubusercontent.com/17ejtcs031/flutter-part1/master/vimal%20sir.JPG
